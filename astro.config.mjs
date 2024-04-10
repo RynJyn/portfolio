3657e343474f818ff89332d52c063bbf503673d1
Ryan Jeynes <ryanjeynes@outlook.com>
2024-04-10 13:08:27 +0100

    Set up typings and definitions of global variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ryanjeynes.co.uk",
+  vite: {
+    define: {
+      __linkedin: JSON.stringify("https://www.linkedin.com/in/ryanjeynes/"),
+      __email: JSON.stringify("ryanjeynes@outlook.com"),
+      __github: JSON.stringify("https://github.com/RynJyn/")
+    },
+  },
   integrations: [icon()],
   output: "server",
   adapter: netlify()
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare const __linkedin: string
+declare const __email: string
+declare const __github: string
